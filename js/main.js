18e02c0150434af09a800ec68957e4dea339419c
mcdowall75 <i.mcdowall@sheffield.ac.uk>
2018-04-10 20:36:38 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ createRestaurantHTML = (restaurant) => {
   picture.append(image);
   li.append(picture);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -101,7 +101,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   title.tabIndex = 0;
   container.appendChild(title);
@@ -160,6 +160,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('aria-current','page');
   breadcrumb.appendChild(li);
 }
 
